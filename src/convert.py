155b780253bd06d4f8e08ca62c3092f31ec1b9b8
Akira Sosa <akirasosa@gmail.com>
2019-10-10 14:59:52 +0700

    convert res18

--- src/convert.py
+++ src/convert.py
@@ -20,14 +20,14 @@ if False:
     # torch.save(net.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_torch.pth')
     torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_trt.pth')
 
-    # %%
-    net = msra_resnet.get_pose_net(18, heads, head_conv=64).eval().cuda()
-    x = torch.ones((1, 3, 512, 512)).cuda()
-    net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
+# %%
+net = msra_resnet.get_pose_net(18, heads, head_conv=64).eval().cuda()
+x = torch.ones((1, 3, 512, 512)).cuda()
+net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
 
-    # %%
-    # torch.save(net.state_dict(), Path.home() / 'tmp' / 'res18_no-head_torch.pth')
-    torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt.pth')
+# %%
+# torch.save(net.state_dict(), Path.home() / 'tmp' / 'res18_no-head_torch.pth')
+torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt.pth')
 
 # %%
 net = efficientnet_centernet.get_pose_net(0, heads, head_conv=64).eval().cuda()
