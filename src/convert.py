5e070ca621580cbc24c5d03b22fd99473a9e3e98
Akira Sosa <akirasosa@gmail.com>
2019-10-11 15:29:38 +0700

    killed

--- src/convert.py
+++ src/convert.py
@@ -12,36 +12,37 @@ from models.networks import efficientnet_centernet, msra_resnet, mobilenet_dcn
 heads = {'hm': 1, 'wh': 2, 'hps': 34, 'reg': 2, 'hm_hp': 17, 'hp_offset': 2}
 
 # %%
-net = mobilenet_dcn.get_pose_net(0, heads, head_conv=64).eval().cuda()
-x = torch.ones((1, 3, 512, 512)).cuda()
-net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
+# net = mobilenet_dcn.get_pose_net(0, heads, head_conv=64).eval().cuda()
+# x = torch.ones((1, 3, 512, 512)).cuda()
+# net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
 
 # %%
-torch.save(net.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_torch.pth')
-torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_trt.pth')
-
-net = net.half()
-x = x.half()
-net_trt_half = torch2trt(net, [x], max_workspace_size=1 << 25, fp16_mode=True)
-
-torch.save(net_trt_half.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_trt_half.pth')
+# torch.save(net.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_torch.pth')
+# torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_trt.pth')
+#
+# net = net.half()
+# x = x.half()
+# net_trt_half = torch2trt(net, [x], max_workspace_size=1 << 25, fp16_mode=True)
+#
+# torch.save(net_trt_half.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_trt_half.pth')
 
 # %%
 net = msra_resnet.get_pose_net(18, heads, head_conv=64).eval().cuda()
 # net = load_model(net, Path.home() / 'data' / 'model_best.pth')
 # net = load_model(net, Path.home() / 'tmp' / 'res18_torch_best.pth')
 x = torch.ones((1, 3, 512, 512)).cuda()
-net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
+# net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
 
 # %%
-torch.save(net.state_dict(), Path.home() / 'tmp' / 'res18_no-head_torch.pth')
-torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt.pth')
+# torch.save(net.state_dict(), Path.home() / 'tmp' / 'res18_no-head_torch.pth')
+# torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt.pth')
 
 net = net.half()
 x = x.half()
 net_trt_half = torch2trt(net, [x], max_workspace_size=1 << 25, fp16_mode=True)
 
 torch.save(net_trt_half.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt_half.pth')
+return
 
 # %%
 net = efficientnet_centernet.get_pose_net(0, heads, head_conv=64)
