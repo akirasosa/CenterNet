b5a95a97e0646186a951e645efecb86440817ac5
Akira Sosa <akirasosa@gmail.com>
2019-10-16 15:34:41 +0700

    onnx export

--- src/convert.py
+++ src/convert.py
@@ -12,11 +12,13 @@ from models.networks import efficientnet_centernet, msra_resnet, mobilenet_dcn
 heads = {'hm': 1, 'wh': 2, 'hps': 34, 'reg': 2, 'hm_hp': 17, 'hp_offset': 2}
 
 # %%
-# net = mobilenet_dcn.get_pose_net(0, heads, head_conv=64).eval().cuda()
-# x = torch.ones((1, 3, 512, 512)).cuda()
+net = mobilenet_dcn.get_pose_net(0, heads, head_conv=64).eval().cuda()
+x = torch.ones((1, 3, 512, 512)).cuda()
 # net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
 
+
 # %%
+torch.onnx.export(net, x, Path.home() / 'tmp' / 'mobilenet_centernet.onnx', verbose=True)
 # torch.save(net.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_torch.pth')
 # torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'mobilenet_dcn_no-head_trt.pth')
 #
@@ -34,8 +36,9 @@ x = torch.ones((1, 3, 512, 512)).cuda()
 net_trt = torch2trt(net, [x], max_workspace_size=1 << 25)
 
 # %%
+torch.onnx.export(net, x, Path.home() / 'tmp' / 'resnet_centernet.onnx')
 # torch.save(net.state_dict(), Path.home() / 'tmp' / 'res18_no-head_torch.pth')
-torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt.pth')
+# torch.save(net_trt.state_dict(), Path.home() / 'tmp' / 'res18_no-head_trt.pth')
 
 # net = net.half()
 # x = x.half()
