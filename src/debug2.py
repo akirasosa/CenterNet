3f08a87081c3fc07fea224b26599d41ca2723252
Akira Sosa <akirasosa@gmail.com>
2019-10-23 13:27:19 +0700

    WIP refactor _gather_feat

--- src/debug2.py
+++ src/debug2.py
@@ -64,6 +64,13 @@ def _nms(heat, kernel=3):
 def _gather_feat(feat, ind):
     dim = feat.size(2)
     ind = ind.unsqueeze(2).expand(ind.size(0), ind.size(1), dim)
+    print(ind.size(0), ind.size(1), dim)
+    feat = feat.gather(1, ind)
+    return feat
+
+
+def _gather_feat2(feat, ind, shape):
+    ind = ind.unsqueeze(2).expand(*shape)
     feat = feat.gather(1, ind)
     return feat
 
@@ -89,8 +96,8 @@ class PostProcess(nn.Module):
         self.hp_offset = hp_offset
         self.K = K
 
-    def _topk(self, scores, K=40):
-        batch, cat, height, width = self.batch, self.hm, self.height, self.width
+    def _topk(self, scores):
+        batch, cat, height, width, K = self.batch, self.hm, self.height, self.width, self.K
 
         topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
 
@@ -100,14 +107,15 @@ class PostProcess(nn.Module):
 
         topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
         topk_clses = (topk_ind / K).int()
-        topk_inds = _gather_feat(topk_inds.view(batch, -1, 1), topk_ind).view(batch, K)
-        topk_ys = _gather_feat(topk_ys.view(batch, -1, 1), topk_ind).view(batch, K)
-        topk_xs = _gather_feat(topk_xs.view(batch, -1, 1), topk_ind).view(batch, K)
+        shape = (batch, K, 1)
+        topk_inds = _gather_feat2(topk_inds.view(batch, -1, 1), topk_ind, shape).view(batch, K)
+        topk_ys = _gather_feat2(topk_ys.view(batch, -1, 1), topk_ind, shape).view(batch, K)
+        topk_xs = _gather_feat2(topk_xs.view(batch, -1, 1), topk_ind, shape).view(batch, K)
 
         return topk_score, topk_inds, topk_clses, topk_ys, topk_xs
 
-    def _topk_channel(self, scores, K=40):
-        batch, cat, height, width = self.batch, self.hm_hp, self.height, self.height
+    def _topk_channel(self, scores):
+        batch, cat, height, width, K = self.batch, self.hm_hp, self.height, self.height, self.K
 
         topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
 
@@ -121,7 +129,7 @@ class PostProcess(nn.Module):
         batch, cat, height, width = self.batch, self.hm, self.height, self.width
         num_joints = kps.shape[1] // 2
         heat = _nms(heat)
-        scores, inds, clses, ys, xs = self._topk(heat, K=K)
+        scores, inds, clses, ys, xs = self._topk(heat)
 
         kps = _tranpose_and_gather_feat(kps, inds)
         kps = kps.view(batch, K, num_joints * 2)
@@ -145,29 +153,25 @@ class PostProcess(nn.Module):
         kps = kps.view(batch, K, num_joints, 2).permute(
             0, 2, 1, 3).contiguous()  # b x J x K x 2
         reg_kps = kps.unsqueeze(3).expand(batch, num_joints, K, K, 2)
-        hm_score, hm_inds, hm_ys, hm_xs = self._topk_channel(hm_hp, K=K)  # b x J x K
-        if hp_offset is not None:
-            hp_offset = _tranpose_and_gather_feat(
-                hp_offset, hm_inds.view(batch, -1))
-            hp_offset = hp_offset.view(batch, num_joints, K, 2)
-            hm_xs = hm_xs + hp_offset[:, :, :, 0]
-            hm_ys = hm_ys + hp_offset[:, :, :, 1]
-        else:
-            hm_xs = hm_xs + 0.5
-            hm_ys = hm_ys + 0.5
+        hm_score, hm_inds, hm_ys, hm_xs = self._topk_channel(hm_hp)  # b x J x K
+        hp_offset = _tranpose_and_gather_feat(hp_offset, hm_inds.view(batch, -1))
+        hp_offset = hp_offset.view(batch, num_joints, K, 2)
+        hm_xs = hm_xs + hp_offset[:, :, :, 0]
+        hm_ys = hm_ys + hp_offset[:, :, :, 1]
 
         mask = (hm_score > thresh).float()
         hm_score = (1 - mask) * -1 + mask * hm_score
         hm_ys = (1 - mask) * (-10000) + mask * hm_ys
         hm_xs = (1 - mask) * (-10000) + mask * hm_xs
-        hm_kps = torch.stack([hm_xs, hm_ys], dim=-1).unsqueeze(
-            2).expand(batch, num_joints, K, K, 2)
+        hm_kps = torch.stack([hm_xs, hm_ys], dim=-1) \
+            .unsqueeze(2) \
+            .expand(batch, num_joints, K, K, 2)
         dist = (((reg_kps - hm_kps) ** 2).sum(dim=4) ** 0.5)
         min_dist, min_ind = dist.min(dim=3)  # b x J x K
         hm_score = hm_score.gather(2, min_ind).unsqueeze(-1)  # b x J x K x 1
         min_dist = min_dist.unsqueeze(-1)
-        min_ind = min_ind.view(batch, num_joints, K, 1, 1).expand(
-            batch, num_joints, K, 1, 2)
+        min_ind = min_ind.view(batch, num_joints, K, 1, 1) \
+            .expand(batch, num_joints, K, 1, 2)
         hm_kps = hm_kps.gather(3, min_ind)
         hm_kps = hm_kps.view(batch, num_joints, K, 2)
         l = bboxes[:, :, 0].view(batch, 1, K, 1).expand(batch, num_joints, K, 1)
@@ -179,8 +183,9 @@ class PostProcess(nn.Module):
                (hm_score < thresh) + (min_dist > (torch.max(b - t, r - l) * 0.3))
         mask = (mask > 0).float().expand(batch, num_joints, K, 2)
         kps = (1 - mask) * hm_kps + mask * kps
-        kps = kps.permute(0, 2, 1, 3).contiguous().view(
-            batch, K, num_joints * 2)
+        kps = kps.permute(0, 2, 1, 3) \
+            .contiguous() \
+            .view(batch, K, num_joints * 2)
         detections = torch.cat([bboxes, scores, kps, clses], dim=2)
 
         return detections
